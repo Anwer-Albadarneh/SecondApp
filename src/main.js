20c7b52ec60a583b5e1d49f83570e0ee47c7b8db
Anwer-Albadarneh <anweralbadarneh@gmail.com>
2023-09-28 04:30:13 +0300

    new project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import data from './data.json';
 import Cardcomp from './card';
+import './card.css';
 
 function Main(){
     return(
         <>
-        <div style={{display:"flex", flexWrap:"wrap", justifyContent:"space-between", gap:"20px", marginTop:"3%"}}>
+        <div id='design'>
         {data.map(function(Food){
             return(
                 <Cardcomp image={Food.image_url} title={Food.title} description={Food.description}/>
